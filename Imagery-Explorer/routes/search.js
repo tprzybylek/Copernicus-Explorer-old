9ce1480b4c2c3a71620ac304314c4f69c3019923
Student 202747 <202747@student.pwr.edu.pl>
2017-08-09 10:27:04 +0200

    fix get query handler handlers for the Sentinel-2 satellite

--- Imagery-Explorer/routes/search.js
+++ Imagery-Explorer/routes/search.js
@@ -12,66 +12,159 @@ router.get('/', function (req, res) {
     // TODO: get request handler
     // TODO: get records
 
+    var datefrom = req.query.dataod;
+
+    var dateto = req.query.datado;
+
     var satellite = req.query.satellite;
-    var orbitdirection = req.query.orbitdirection;
-    var producttype = req.query.producttype;
-    var mode = req.query.mode;
-    var polarisationmode = req.query.polarisationmode;
-    var relativeorbitnumber = req.query.relativeorbitnumber;
+
+    var orbitdirectionS1 = req.query.orbitdirectionS1;
+    var producttypeS1 = req.query.producttypeS1;
+    var modeS1 = req.query.modeS1;
+    var polarisationmodeS1 = req.query.polarisationmodeS1;
+    var relativeorbitnumberS1 = req.query.relativeorbitnumberS1;
+
+    var orbitdirectionS2 = req.query.orbitdirectionS2;
+    var relativeorbitnumberS2 = req.query.relativeorbitnumberS2;
 
     var query = {};
 
+    var dataspace = {};
+
+    if (datefrom != '') {
+        datefrom = 'ISODate(' + datefrom + 'T00:00:00.000Z)';
+        dataspace['$gte'] = datefrom
+        query['properties.ingestiondate'] = dataspace;
+    };
+
+    if (dateto != '') {
+        dateto = 'ISODate(' + dateto + 'T23:59:59.999Z)';
+        dataspace['$lt'] = dateto
+        query['properties.ingestiondate'] = dataspace;
+    };
+
     if (satellite == 'S1') {
-        query['$or'] = [{ 'properties.satellite': 'S1A' }, { 'properties.satellite': 'S1B' }]
+        query['properties.satellite'] = new RegExp('S1')
+
+        if (orbitdirectionS1 == 'ASCENDING') {
+            query['properties.orbitdirection'] = 'ASCENDING'
+        } else if (orbitdirectionS1 == 'DESCENDING') {
+            query['properties.orbitdirection'] = 'DESCENDING'
+        } else {
+            //do nothing
+        };
+
+        if (producttypeS1 == 'GRD') {
+            query['properties.producttype'] = 'GRD'
+        } else if (producttypeS1 == 'SLC') {
+            query['properties.producttype'] = 'SLC'
+        } else if (producttypeS1 == 'RAW') {
+            query['properties.producttype'] = 'RAW'
+        } else {
+            //do nothing
+        };
+
+        if (polarisationmodeS1 == 'VV') {
+            query['properties.polarisationmode'] = new RegExp('VV')
+        } else if (polarisationmodeS1 == 'HV') {
+            query['properties.polarisationmode'] = new RegExp('HV')
+        } else if (polarisationmodeS1 == 'VH') {
+            query['properties.polarisationmode'] = new RegExp('VH')
+        } else if (polarisationmodeS1 == 'HH') {
+            query['properties.polarisationmode'] = new RegExp('HH')
+        } else {
+            //do nothing
+        };
+
+        if (modeS1 == 'IW') {
+            query['properties.mode'] = 'IW'
+        } else if (modeS1 == 'SM') {
+            query['properties.mode'] = 'SM'
+        } else if (modeS1 == 'EW') {
+            query['properties.mode'] = 'EW'
+        } else {
+            //do nothing
+        };
+
+        if (relativeorbitnumberS1) {
+            query['properties.relativeorbitnumber'] = relativeorbitnumberS1
+        };
+
     } else if (satellite == 'S2') {
-        query['$or'] = [{ 'properties.satellite': 'S2A' }, { 'properties.satellite': 'S2B' }]
+        query['properties.satellite'] = new RegExp('S2')
+
+        if (orbitdirectionS2 == 'ASCENDING') {
+            query['properties.orbitdirection'] = 'ASCENDING'
+        } else if (orbitdirectionS2 == 'DESCENDING') {
+            query['properties.orbitdirection'] = 'DESCENDING'
+        } else {
+            //do nothing
+        };
+
+        if (relativeorbitnumberS2) {
+            query['properties.relativeorbitnumber'] = relativeorbitnumberS2
+        };
+
     } else {
-        //do nothing
+        query['$or'] = [{ 'properties.satellite': new RegExp('S1') }, { 'properties.satellite': new RegExp('S2') }]
+        if (orbitdirectionS1 == 'ASCENDING') {
+            query['$or'][0]['properties.orbitdirection'] = 'ASCENDING'
+        } else if (orbitdirectionS1 == 'DESCENDING') {
+            query['$or'][0]['properties.orbitdirection'] = 'DESCENDING'
+        } else {
+            //do nothing
+        };
+
+        if (producttypeS1 == 'GRD') {
+            query['$or'][0]['properties.producttype'] = 'GRD'
+        } else if (producttypeS1 == 'SLC') {
+            query['$or'][0]['properties.producttype'] = 'SLC'
+        } else if (producttypeS1 == 'RAW') {
+            query['$or'][0]['properties.producttype'] = 'RAW'
+        } else {
+            //do nothing
+        };
+
+        if (polarisationmodeS1 == 'VV') {
+            query['$or'][0]['properties.polarisationmode'] = new RegExp('VV')
+        } else if (polarisationmodeS1 == 'HV') {
+            query['$or'][0]['properties.polarisationmode'] = new RegExp('HV')
+        } else if (polarisationmodeS1 == 'VH') {
+            query['$or'][0]['properties.polarisationmode'] = new RegExp('VH')
+        } else if (polarisationmodeS1 == 'HH') {
+            query['$or'][0]['properties.polarisationmode'] = new RegExp('HH')
+        } else {
+            //do nothing
+        };
+
+        if (modeS1 == 'IW') {
+            query['$or'][0]['properties.mode'] = 'IW'
+        } else if (modeS1 == 'SM') {
+            query['$or'][0]['properties.mode'] = 'SM'
+        } else if (modeS1 == 'EW') {
+            query['$or'][0]['properties.mode'] = 'EW'
+        } else {
+            //do nothing
+        };
+
+        if (relativeorbitnumberS1 != '') {
+            query['$or'][0]['properties.relativeorbitnumber'] = relativeorbitnumberS1
+        };
+
+        if (orbitdirectionS2 == 'ASCENDING') {
+            query['$or'][1]['properties.orbitdirection'] = 'ASCENDING'
+        } else if (orbitdirectionS2 == 'DESCENDING') {
+            query['$or'][1]['properties.orbitdirection'] = 'DESCENDING'
+        } else {
+            //do nothing
+        };
+
+        if (relativeorbitnumberS2 != '') {
+            query['$or'][1]['properties.relativeorbitnumber'] = relativeorbitnumberS2
+        };
     };
 
-    if (orbitdirection == 'ASCENDING') {
-        query['properties.orbitdirection'] = 'ASCENDING'
-    } else if (orbitdirection == 'DESCENDING') {
-        query['properties.orbitdirection'] = 'DESCENDING'
-    } else {
-        //do nothing
-    };
-
-    if (producttype == 'GRD') {
-        query['properties.producttype'] = 'GRD'
-    } else if (producttype == 'SLC') {
-        query['properties.producttype'] = 'SLC'
-    } else if (producttype == 'RAW') {
-        query['properties.producttype'] = 'RAW'
-    } else {
-        //do nothing
-    };
-
-    if (polarisationmode == 'VV') {
-        query['properties.polarisationmode'] = new RegExp('VV')
-    } else if (polarisationmode == 'HV') {
-        query['properties.polarisationmode'] = new RegExp('HV')
-    } else if (polarisationmode == 'VH') {
-        query['properties.polarisationmode'] = new RegExp('VH')
-    } else if (polarisationmode == 'HH') {
-        query['properties.polarisationmode'] = new RegExp('HH')
-    } else {
-        //do nothing
-    };
-
-    if (mode == 'IW') {
-        query['properties.mode'] = 'IW'
-    } else if (mode == 'SM') {
-        query['properties.mode'] = 'SM'
-    } else if (mode == 'EW') {
-        query['properties.mode'] = 'EW'
-    } else {
-        //do nothing
-    };
- 
-    if (relativeorbitnumber) {
-        query['properties.relativeorbitnumber'] = relativeorbitnumber
-    }
+    
 
     console.log(query);
 
