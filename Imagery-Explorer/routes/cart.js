08f9a954ce100b251a950320779f151bd0001066
Student 202747 <202747@student.pwr.edu.pl>
2017-09-07 15:21:02 +0200

    Fix cart feature

--- Imagery-Explorer/routes/cart.js
+++ Imagery-Explorer/routes/cart.js
@@ -13,22 +13,65 @@ var pool = mysql.createPool({
 
 router.get('/', function (req, res) {
 
-    var IDs = req.cookies.IDs.split(",");
+    var IDs = JSON.parse(req.cookies.IDs);
 
-    var query = '';
+    if (IDs.length > 0) { 
 
-    pool.getConnection(function (err, connection) {
-        if (err) throw err;
-        console.log('Connection ID: ' + connection.threadId);
+        results = [];
 
-        connection.query("SELECT * FROM (SELECT * FROM s1 UNION SELECT * FROM s2) AS U WHERE U.ID = ?", IDs[0] ,function (err, result, fields) {
+        /*
+        var myCallback = function (data) {
+            console.log('got data: ' + data);
+        };
+
+        var usingItNow = function (callback) {
+            callback('get it?');
+        };
+
+        usingItNow(myCallback);
+        */
+
+        /*
+        var pushResult = function (data) {
+            console.log('got data: ' + result);
+        };
+
+        var queryDB = function (callback) {
+
+            connection.query("SELECT * FROM (SELECT * FROM s1 UNION SELECT * FROM s2) AS U WHERE U.ID = ?", IDs[0], function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                //res.render('cart', { title: 'Koszyk', items: results });
+                callback(results);
+                connection.release();
+            });
+            
+        };
+        */
+
+        queryDB(pushResult);
+        
+
+
+
+
+
+
+        pool.getConnection(function (err, connection) {
             if (err) throw err;
-            console.log(result);
-            res.render('cart', { title: 'cart', items: result });
-            connection.release();
+            console.log('Connection ID: ' + connection.threadId);
+
+            connection.query("SELECT * FROM (SELECT * FROM s1 UNION SELECT * FROM s2) AS U WHERE U.ID = ?", IDs[0] ,function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                res.render('cart', { title: 'Koszyk', items: results });
+                connection.release();
+            });
         });
 
-    });
+    } else {
+        res.render('cart', { title: 'Koszyk'});
+    };
 
     console.log('IDs: ', IDs)
 });
