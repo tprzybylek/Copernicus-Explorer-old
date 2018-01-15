8fc51a3a7625dd05b53b4623ef83432e6908683d
Student 202747 <202747@student.pwr.edu.pl>
2018-01-15 09:42:16 +0100

    Cleanup

--- Scripts/Untitled-1.js
+++
@@ -1,5 +0,0 @@
-function JSONtoWKT(extent){
-    var wkt = 'POLYGON(('
-    wkt = wkt + extent['minX'] + ' ' + extent['maxY'] + ',' + extent['maxX'] + ' ' + extent['maxY'] + ',' + extent['maxX'] + ' ' + extent['minY'] + ',' + extent['minX'] + ' ' + extent['minY'] + ',' + extent['minX'] + ' ' + extent['maxY'] + '))'
-    return wkt
-};
